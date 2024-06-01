e45c860fd510704a7a1a88e4004ce2b652aaac75
minhtuyenvp02 <119645277+minhtuyenvp02@users.noreply.github.com>
2024-06-01 19:30:05 +0700

    add slack alert

--- docker-custom-service/airflow/dags/trip_tracking_daily.py
+++ docker-custom-service/airflow/dags/trip_tracking_daily.py
@@ -31,33 +31,39 @@ start_date = datetime(2024, 5, 30)
 SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 
 
-def alert_slack_channel(context):
+def alert_slack_channel(context: dict):
     """ Alert to slack channel on failed dag
+
     :param context: airflow context object
     """
     if not SLACK_WEBHOOK_URL:
         # Do nothing if slack webhook not set up
         return
 
-    last_task = context.get('task_instance')
+    last_task: Optional[TaskInstance] = context.get('task_instance')
     dag_name = last_task.dag_id
+    task_name = last_task.task_id
+    error_message = context.get('exception') or context.get('reason')
     execution_date = context.get('execution_date')
     dag_run = context.get('dag_run')
     task_instances = dag_run.get_task_instances()
-    failed_tis = [ti.task_id for ti in task_instances if ti.state == 'failed']
+    file_and_link_template = "<{log_url}|{name}>"
+    failed_tis = [file_and_link_template.format(log_url=ti.log_url, name=ti.task_id)
+                  for ti in task_instances
+                  if ti.state == 'failed']
     title = f':red_circle: Dag: *{dag_name}* has failed, with ({len(failed_tis)}) failed tasks'
     msg_parts = {
         'Execution date': execution_date,
         'Failed Tasks': ', '.join(failed_tis),
+        'Error': error_message
     }
     msg = "\n".join([title, *[f"*{key}*: {value}" for key, value in msg_parts.items()]]).strip()
 
-    http_hook = HttpHook(method='POST')
-    headers = {'Content-Type': 'application/json'}
-    payload = json.dumps({'text': msg})
-    response = http_hook.run(endpoint=SLACK_WEBHOOK_URL, data=payload, headers=headers)
-    if response.status_code != 200:
-        raise ValueError(f"Request to Slack returned an error {response.status_code}, the response is:\n{response.text}")
+    SlackWebhookHook(
+        webhook_token=SLACK_WEBHOOK_URL,
+        message=msg,
+    ).execute()
+
 
 default_args = {
     "owner": "airflow",
@@ -68,14 +74,13 @@ default_args = {
     "start_date": start_date,
 }
 with DAG(
-    default_args=default_args,
-    dag_id="trip_tracking_daily",
-    schedule_interval='@daily',
-    tags=["trip_tracking_daily", "medallion_architecture"],
-    on_failure_callback=alert_slack_channel,
-    catchup=False,
+        default_args=default_args,
+        dag_id="trip_tracking_daily",
+        schedule_interval='@daily',
+        tags=["trip_tracking_daily", "medallion_architecture"],
+        on_failure_callback=alert_slack_channel,
+        catchup=False,
 ) as dag:
-
     gold_fact_yellow_tracking = BashOperator(
         task_id="gold_update_yellow_tracking_daily",
         bash_command=f'''
