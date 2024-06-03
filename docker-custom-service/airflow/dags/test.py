33ef7dccdc026d03feb38146d15b1008c09bd3cd
minhtuyenvp02 <119645277+minhtuyenvp02@users.noreply.github.com>
2024-06-04 00:23:37 +0700

    add ignore git

--- docker-custom-service/airflow/dags/test.py
+++
@@ -1,4 +0,0 @@
-import os
-import sys
-sys.path.append("/Users/minhtuyen02/MTuyen/work-space/University/doantotnghiep_2024/Graduation_Thesis_SOICT_2024/docker-custom-service/airflow/scripts/spark")
-from gold_fact_fhvhv_tracking import main
